3b1c1a25048d923f2a6303977b2f56dea92d86e8
NourElshabasy <nourelshabasy06@gmail.com>
2026-06-25 16:32:12 -0400

    Add RedditPost model and implement async Main method for API call

---
+++ Models/RedditPost.cs
@@ -0,0 +1,8 @@
+namespace Models;
+
+internal class RedditPost
+{
+    public string? Title { get; set; }
+    public string? Score { get; set; }
+    public string? Author { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Net.Http;
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
+        using (var client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.Add(
+                "User-Agent",
+                "RedditAnalyzer/1.0"
+            );
+
+            var response = await client.GetAsync("https://www.reddit.com/r/programming.json");
+            var json = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(json);
+        }
     }
 }
